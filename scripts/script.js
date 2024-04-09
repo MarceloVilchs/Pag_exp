a612493445f3353bceac870fcca18d48cd4e92da
MarceloVilchs <m.vilchess@duocuc.cl>
2024-04-08 20:26:53 -0400

    registro y login con sus fomularios

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById("compro");
+
+function mostrarCartel(){
+    boton.innerHTML = "Compró el producto"
+}
+
+function mostrarCartelf(){
+    boton.innerHTML = "Ha sido añadido a favoritos!"
+}
+
